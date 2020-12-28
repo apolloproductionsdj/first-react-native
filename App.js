47afd818e114051775e785b1f34daa51ae5722b6
mike <apolloproductionsdj@gmail.com>
2020-12-28 14:18:54 -0700

    made first component with input functionality

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import Todo from './Todo';
 
 const App = () => {
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    setTodos([input, ...todos]);
+    setInput('');
+  }
+
   return (
     <SafeAreaView>
       <View>
         <Text style={styles.titleText}>Let's build a React Native App in One Day 🚀 🙂 🚢  </Text>
       </View>
-      <Todo title="Take the trash out" />
-      <Todo title="Code React Native TinDev App" />
-      <Todo title="Eat, Code, Sleep" />
+      <ScrollView>
+        {todos.map((todo) => (
+          <Todo title={todo} />
+        ))}
+      </ScrollView>
+      <TextInput
+        style={styles.todoInput}
+        value={input}
+        onChangeText={text => setInput(text)}
+      />
+      <Button onPress={addTodo} title="ADD TODO" />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  todoInput: {
+    margin: 20,
+    height: 40,
+    borderColor: 'red',
+    borderWidth: 1
+  },
   titleText: {
     backgroundColor: 'red',
     fontSize: 30,
